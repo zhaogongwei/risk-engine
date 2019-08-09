149386de5865fde4dffa92cfc97881d567a1c31a
zhaogongwei <zhaogongwei@shizitegong.com>
2019-08-09 17:14:06 +0800

    风控引擎>策略流编辑页面>简单规则页面table设置筛选功能

--- src/pages/PolicyManage/RiskPolicyList/PolicyFlowList/PolicyTestTemp/PolicyFlowEdit/Rule/index.js
+++ src/pages/PolicyManage/RiskPolicyList/PolicyFlowList/PolicyTestTemp/PolicyFlowEdit/Rule/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent, Fragment } from 'react';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+import Highlighter from 'react-highlight-words';
 import {
   Row,
   Col,
@@ -11,7 +12,8 @@ import {
   Form,
   Popconfirm,
   Modal,
-  Card
+  Card,
+  Input,
 } from 'antd';
 import { connect } from 'dva'
 // 验证权限的组件
@@ -46,8 +48,7 @@ export default class SimpleRule extends PureComponent {
         type:'input',
         isFocus:true,
         cols:1,
-        sorter: (a, b) => a.varName.length - b.varName.length,
-        sortDirections: ['ascend'],
+          ...this.getColumnSearchProps('varName'),
       },{
         title: '变量代码',
         dataIndex: 'varCode',
@@ -138,6 +139,7 @@ export default class SimpleRule extends PureComponent {
       isCount:0,//计数结果类型判断
       resultVarId:{},//输出结果
       countResult:{},//计数结果
+      searchText: '',
     };
   }
  async componentDidMount () {
@@ -298,6 +300,66 @@ export default class SimpleRule extends PureComponent {
         }
       })
   }
+  //table 按变量名称搜索
+  getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: text => (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[this.state.searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+    ),
+  });
+
+  handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    this.setState({ searchText: selectedKeys[0] });
+  };
+
+  handleReset = clearFilters => {
+    clearFilters();
+    this.setState({ searchText: '' });
+  };
   render() {
     const { permission } = this.props
     const { getFieldDecorator } = this.props.form
