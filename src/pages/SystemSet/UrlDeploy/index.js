d99015001cb640a552c67804e43a45e5fe67ecc7
bloveAmber <bloveAmber.9@gmail.com>
2019-08-12 11:57:02 +0800

    系统设置 接口配置代码修改

--- src/pages/SystemSet/UrlDeploy/index.js
+++ src/pages/SystemSet/UrlDeploy/index.js
@@ -21,9 +21,9 @@ import AddForm from './addForm';
 import DropdownDetail from '@/components/DropdownDetail/DropdownDetail'
 import { findInArr,exportJudgment } from '@/utils/utils'
 
-@connect(({ assetDeploy, loading }) => ({
-  assetDeploy,
-  loading: loading.effects['assetDeploy/riskSubmit']
+@connect(({ urldeploy, loading }) => ({
+  urldeploy,
+  loading: loading.effects['urldeploy/fetchUrlList']
 }))
 export default class UrlDeploy extends PureComponent {
   constructor(props) {
@@ -87,55 +87,33 @@ export default class UrlDeploy extends PureComponent {
       ],
       checkedData: [],
       modalStatus:false,
-      code:'',
       type:1,//1:添加 2：编辑
       pageSize:10,
-      currentPage:1,
-      current:1,
-      id:'',
+      currPage:1,
       status:1,
-      record:{},
-      visible:false,
-      isTrust:0,//授权状态框显示状态
     };
   }
   componentDidMount() {
     this.change()
   }
   //  分页器改变页数的时候执行的方法
-  onChange = (current) => {
+  onChange = (currPage,pageSize) => {
     this.setState({
-      current:current,
-      currentPage:current
+      currPage,
+      pageSize
     })
-    this.change(current)
+    this.change(currPage,pageSize)
   }
   // 进入页面去请求页面数据
   change = (currPage = 1, pageSize = 10) => {
-    let formData ;
-    if(this.child){
-      formData = this.child.getFormValue()
-    }else{
-      formData = {}
-    }
     this.props.dispatch({
-      type: 'assetDeploy/riskSubmit',
+      type: 'urldeploy/fetchUrlList',
       data: {
-        ...formData,
         currPage,
         pageSize
       }
     })
     // this.refs.paginationTable && this.refs.paginationTable.setPagiWidth()
-  }
-  confirm=(e)=>{
-    console.log(e);
-    message.success('Click on Yes');
-  }
-
-  cancel=(e) =>{
-    console.log(e);
-    message.error('Click on No');
   }
   //   获取子组件数据的方法
   getSubKey=(ref,key)=>{
@@ -155,24 +133,18 @@ export default class UrlDeploy extends PureComponent {
   reload = () => {
     window.location.reload();
   }
-  //查询时改变默认页数
-  changeDefault=(value)=>{
-    this.setState({
-      current:value
-    })
-  }
   //右上角渲染
   renderTitleBtn = () => {
     return (
       <Fragment>
-        <Button onClick={()=>this.addEditPage(1)}><Icon type="plus" theme="outlined" />新增</Button>
+        <Button onClick={()=>this.addEditPage(true, 1)}><Icon type="plus" theme="outlined" />新增</Button>
       </Fragment>
     )
   }
-  addEditPage=(type)=>{
+  addEditPage=(flag,type)=>{
     this.setState({
-      type:type,
-      visible:true,
+      type,
+      visible: !!flag,
     })
   }
   //弹框点击确定事件
@@ -200,11 +172,17 @@ export default class UrlDeploy extends PureComponent {
     }
   }
   render() {
+    const { type } = this.state;
+    const modalParams = {
+      type: this.state.type,
+      visible: this.state.visible,
+      addEditPage: this.addEditPage
+    }
     return (
      <PageHeaderWrapper  renderBtn={this.renderTitleBtn}>
        <Card
-          bordered={false}
-          title={'接口配置'}
+        bordered={false}
+        title={'接口配置'}
        >
          <Table
            bordered
@@ -217,23 +195,18 @@ export default class UrlDeploy extends PureComponent {
            style={{ marginBottom: "50px" }}
            showQuickJumper
            defaultCurrent={1}
-           current={this.state.current}
+           current={this.state.currPage}
            total={100}
            onChange={this.onChange}
            showTotal={(total, range) => this.showTotal(total, range)}
          />
-         <Modal
-           title={this.state.type===1?'新增接口':'修改接口'}
-           visible={this.state.visible}
-           onOk={this.addFormSubmit}
-           onCancel={()=>this.setState({visible:false})}
-         >
-         <AddForm
-           getSubKey={this.getSubKey}
-           type={this.state.type}
-           record={this.state.record}
-         />
-         </Modal>
+         {
+           this.state.visible ? 
+           <AddForm
+            getSubKey={this.getSubKey}
+            {...modalParams}
+          /> : null
+         }
        </Card>
       </PageHeaderWrapper>
     )
